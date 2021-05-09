00550c17088d4926bb74d69fde6f1077e8891c05
Igor Silva <igorhms@hotmail.com>
2021-05-08 21:42:05 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -14,11 +14,6 @@ const DonutChart = () => {
 
     const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    /*const mockData = {
-        series: [477138, 499928, 444867, 220426, 473088],
-        labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-    }*/
-
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
             .then((response) => {
